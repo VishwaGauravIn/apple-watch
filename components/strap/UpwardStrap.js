d18abd94708035afa3b8f92c5983a382bd8f1ce8
Vishwa Gaurav <vishwagauravin@gmail.com>
2022-01-20 00:33:12 +0530

    theme fixes

--- components/strap/UpwardStrap.js
+++ components/strap/UpwardStrap.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function UpwardStrap() {
   return (
     <div className="w-[20rem] aspect-[1/1.5] relative left-1/2 -translate-x-1/2 shadow-2xl">
-      <div className="absolute left-0 w-2 h-full strap brightness-90 shadow-2xl z-10"></div>
-      <div className="absolute right-0 w-2 h-full strap brightness-90 shadow-2xl z-10"></div>
-      <div className="w-full h-full strap shadow-2xl"></div>
+      <div className="absolute left-0 w-2 h-full strap-updward brightness-75 shadow-2xl z-10"></div>
+      <div className="absolute right-0 w-2 h-full strap-updward brightness-75 shadow-2xl z-10"></div>
+      <div className="w-full h-full strap-updward shadow-2xl brightness-[0.8]"></div>
     </div>
   );
 }
