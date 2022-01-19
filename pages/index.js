1c3f8d0586fe25734260d43b1957802ded8d9cb4
Vishwa Gaurav <vishwagauravin@gmail.com>
2022-01-20 02:05:51 +0530

    added meta tags

--- pages/index.js
+++ pages/index.js
@@ -6,28 +6,54 @@ import Strap from "../components/strap/Strap";
 import UpwardStrap from "../components/strap/UpwardStrap";
 
 export default function Home() {
-    let time = new Date();
-    let [hour, setHour] = useState();
-    let [minute, setMinute] = useState();
-    const updateTime = () => {
-      time = new Date();
-      setHour(n(time.getHours()))
-      setMinute(n(time.getMinutes()))
-    }
-    function n(n) {
-      return n > 9 ? "" + n : "0" + n;
-    }
-    setInterval(updateTime, 1000);
+  let time = new Date();
+  let [hour, setHour] = useState();
+  let [minute, setMinute] = useState();
+  const updateTime = () => {
+    time = new Date();
+    setHour(n(time.getHours()));
+    setMinute(n(time.getMinutes()));
+  };
+  function n(n) {
+    return n > 9 ? "" + n : "0" + n;
+  }
+  setInterval(updateTime, 1000);
   return (
     <>
       <Head>
         <title>Apple Watch</title>
+        <meta name="title" content="Apple Watch" />
         <meta
           name="description"
           content="Apple Watch build with NextJS (ReactJS) & TailwindCSS"
         />
+        <meta name="copyright" content="VishwaGauravIn" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://apple-watch.itsvg.in" />
+        <meta property="og:title" content="Apple Watch" />
+        <meta
+          property="og:description"
+          content="Apple Watch build with NextJS (ReactJS) & TailwindCSS"
+        />
+        <meta
+          property="og:image"
+          content="https://apple-watch.itsvg.in/webimg.png"
+        />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://apple-watch.itsvg.in" />
+        <meta property="twitter:title" content="Apple Watch" />
+        <meta
+          property="twitter:description"
+          content="Apple Watch build with NextJS (ReactJS) & TailwindCSS"
+        />
+        <meta
+          property="twitter:image"
+          content="https://apple-watch.itsvg.in/webimg.png"
+        />
+
         <meta name="viewport" content="width=1366" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.png" />
       </Head>
 
       <body className="min-h-screen">
